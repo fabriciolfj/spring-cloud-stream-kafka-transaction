fa475a4566d8c59d6f16280b4ba97e15e5181c51
fabricio jacob <fabricio.jacob@outlook.com>
2023-11-16 22:43:15 -0300

    add

---
+++ src/main/java/com/fabriciolfj/example/kafkatransaction/KafkaTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.fabriciolfj.example.kafkatransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTransactionApplication.class, args);
+	}
+
+}
